a1cb3b2cc68d3fb33d3cee708b24b2bff643e4cb
Jonathan Karr <jonrkarr@gmail.com>
2020-11-25 15:15:13 -0500

    updating

--- biosimulators_pysces/core.py
+++ biosimulators_pysces/core.py
@@ -6,7 +6,7 @@
 :License: MIT
 """
 
-from Biosimulations_utils.simulation.data_model import Simulation  # noqa: F401
+from Biosimulations_utils.simulation.data_model import TimecourseSimulation, SimulationResultsFormat
 from Biosimulations_utils.simulator.utils import exec_simulations_in_archive
 import os
 
@@ -21,7 +21,7 @@ def exec_combine_archive(archive_file, out_dir):
         archive_file (:obj:`str`): path to COMBINE archive
         out_dir (:obj:`str`): directory to store the outputs of the tasks
     """
-    exec_simulations_in_archive(archive_file, exec_simulation, out_dir)
+    exec_simulations_in_archive(archive_file, exec_simulation, out_dir, apply_model_changes=True)
 
 
 def exec_simulation(model_filename, model_sed_urn, simulation, working_dir, out_filename, out_format):
@@ -30,11 +30,26 @@ def exec_simulation(model_filename, model_sed_urn, simulation, working_dir, out_
     Args:
        model_filename (:obj:`str`): path to the model
        model_sed_urn (:obj:`str`): SED URN for the format of the model (e.g., `urn:sedml:language:sbml`)
-       simulation (:obj:`Simulation`): simulation
+       simulation (:obj:`TimecourseSimulation`): simulation
        working_dir (:obj:`str`): directory of the SED-ML file
        out_filename (:obj:`str`): path to save the results of the simulation
-       out_format (:obj:`str`): format to save the results of the simulation (e.g., `csv`)
+       out_format (:obj:`SimulationResultsFormat`): format to save the results of the simulation (e.g., `HDF5`)
     '''
+    # check that model is encoded in SBML
+    if model_sed_urn != "urn:sedml:language:sbml":
+        raise NotImplementedError("Model language with URN '{}' is not supported".format(model_sed_urn))
+
+    # check that simulation is a time course simulation
+    if not isinstance(simulation, TimecourseSimulation):
+        raise NotImplementedError('{} is not supported'.format(simulation.__class__.__name__))
+
+    # check that model parameter changes have already been applied (because handled by :obj:`exec_simulations_in_archive`)
+    if simulation.model_parameter_changes:
+        raise NotImplementedError('Model parameter changes are not supported')
+
+    # check that the desired output format is supported
+    if out_format != SimulationResultsFormat.HDF5:
+        raise NotImplementedError("Simulation results format '{}' is not supported".format(out_format))
 
     # Read the model located at `os.path.join(working_dir, model_filename)` in the format
     # with the SED URN `model_sed_urn`.
