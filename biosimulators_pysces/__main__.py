360f863f055d4d42056a24edc81e8d8abc45d4a2
Jonathan Karr <jonrkarr@gmail.com>
2020-12-29 23:16:25 -0500

    adding information about supported environment variables to inline help

--- biosimulators_pysces/__main__.py
+++ biosimulators_pysces/__main__.py
@@ -9,6 +9,8 @@
 from ._version import __version__
 from .core import exec_sedml_docs_in_combine_archive
 from biosimulators_utils.simulator.cli import build_cli
+from biosimulators_utils.simulator.data_model import AlgorithmSubstitutionPolicy
+from biosimulators_utils.simulator.environ import ENVIRONMENT_VARIABLES
 import os
 cwd = os.getcwd()  # because PySCeS changes the working directory
 import pysces  # noqa: E402
@@ -16,7 +18,10 @@ os.chdir(cwd)
 
 App = build_cli('pysces', __version__,
                 'PySCeS', pysces.__version__, 'http://pysces.sourceforge.net/',
-                exec_sedml_docs_in_combine_archive)
+                exec_sedml_docs_in_combine_archive,
+                environment_variables=[
+                    ENVIRONMENT_VARIABLES[AlgorithmSubstitutionPolicy]
+                ])
 
 
 def main():
